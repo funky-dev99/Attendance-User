b3d79151b1ff3632ba50fe2d497cc18e3f6ad615
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-05-26 19:24:11 +0530

    Slide Button Added Get Location
    Atten_location.dart edite for decoration and aded Slide button

--- attend_user/lib/pages/attend_location.dart
+++ attend_user/lib/pages/attend_location.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:attend_user/methods/methods.dart';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:intl/intl.dart';
+import 'package:slide_to_act/slide_to_act.dart';
 
 import 'attendance.dart';
 
@@ -16,8 +18,17 @@ class AttendLoc extends StatefulWidget {
 class _AttendLocState extends State<AttendLoc> {
   // TextEditingController dateController = TextEditingController();
   TextEditingController dateController = TextEditingController();
+  Timer? _resetTimer;
+  GlobalKey<SlideActionState> slideActionKey = GlobalKey();
+
 
   @override
+
+  void dispose() {
+    _resetTimer?.cancel();
+    super.dispose();
+  }
+
   void initState() {
     dateController.text = "";
     _timeString = _formatDateTime(DateTime.now());
@@ -34,8 +45,17 @@ class _AttendLocState extends State<AttendLoc> {
 
   LocationData? _userLocation;
 
+  void _startResetTimer() {
+    _resetTimer?.cancel();
+    _resetTimer = Timer(Duration(seconds: 1), () {
+      slideActionKey.currentState?.reset();
+    });
+  }
+
   Future<void> _getUserLocation() async {
     Location location = Location();
+    print('Getting user location...');
+    _startResetTimer();
 
     _serviceEnabled = await location.serviceEnabled();
     if (_serviceEnabled) {
@@ -87,81 +107,98 @@ class _AttendLocState extends State<AttendLoc> {
             onPressed: (){},)
         ],
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: getSizeBoxHeightL(context)),
 
-              Text(_timeString),
+            Text(_timeString,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
 
-              TextField(
+            TextField(
 
 
-                controller: dateController, //editing controller of this TextField
-                decoration: const InputDecoration(
+              controller: dateController, //editing controller of this TextField
+              decoration: const InputDecoration(
 
-                    icon: Icon(Icons.calendar_today), //icon of text field
-                    labelText: "Enter Date" //label text of field
-                ),
-                readOnly: true,  // when true user cannot edit text
-                onTap: () async {
-                  DateTime? pickedDate = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(), //get today's date
-                      firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
-                      lastDate: DateTime(2101)
-                  );
-
-                  if(pickedDate != null ){
-                    print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
-                    String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
-                    print(formattedDate); //formatted date output using intl package =>  2022-07-04
-                    //You can format date as per your need
-
-                    setState(() {
-                      dateController.text = formattedDate; //set foratted date to TextField value.
-                    });
-                  }else{
-                    print("Date is not selected");
-                  }
-                },
+                  icon: Icon(Icons.calendar_today), //icon of text field
+                  labelText: "Enter Date" //label text of field
               ),
+              readOnly: true,  // when true user cannot edit text
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(), //get today's date
+                    firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime(2101)
+                );
+
+                if(pickedDate != null ){
+                  print(pickedDate);  //get the picked date in the format => 2022-07-04 00:00:00.000
+                  String formattedDate = DateFormat('yyyy-MM-dd').format(pickedDate); // format date in required form here we use yyyy-MM-dd that means time is removed
+                  print(formattedDate); //formatted date output using intl package =>  2022-07-04
+                  //You can format date as per your need
+
+                  setState(() {
+                    dateController.text = formattedDate; //set foratted date to TextField value.
+                  });
+                }else{
+                  print("Date is not selected");
+                }
+              },
+            ),
 
 
-              SizedBox(height: 25),
+            SizedBox(height: getSizeBoxHeight(context)),
+
+            SlideAction(
+              onSubmit: _getUserLocation,
+              text: 'Get Your Location',
+              textStyle: TextStyle(color: Colors.white, fontSize: 22),
+              borderRadius: 12,
+              innerColor: Colors.black,
+              outerColor: Colors.grey,
+              sliderButtonIcon: Icon(Icons.location_on,color: Colors.white,),
+
+            ),
 
 
-              ElevatedButton(
-                onPressed: _getUserLocation,
-                child: const Text('Check Location'),
-              ),
-              const SizedBox(height: 25),
-              _userLocation != null
-                  ? Wrap(
-                children: [
-                  Text('Your latitude: ${_userLocation?.latitude}'),
-                  const SizedBox(width: 10),
-                  Text('Your longitude: ${_userLocation?.longitude}'),
-                ],
-              )
-                  : const Text(
-                  'Please enable location service and grant permission'),
+            // ElevatedButton(
+            //   onPressed: _getUserLocation,
+            //   child: const Text('Check Location'),
+            // ),
+            const SizedBox(height: 25),
+            _userLocation != null
+                ? Wrap(
+              children: [
+                Text('Your latitude: ${_userLocation?.latitude}',style: TextStyle(fontSize: 15,color: Colors.blue),),
+                const SizedBox(width: 10),
+                Text('Your longitude: ${_userLocation?.longitude}',style: TextStyle(fontSize: 15,color: Colors.blue),),
+              ],
+            )
+                : const Text(
+                'Please enable location service and grant permission',style: TextStyle(fontSize: 15,color: Colors.red),),
 
-              SizedBox(height: 25),
+            SizedBox(height: 25),
 
-              ElevatedButton(
-                onPressed: (){
-                  print('Latitude: ${_userLocation?.latitude}');
-                  print('Longitude: ${_userLocation?.longitude}');
-                  print('Time:$_timeString');
-                },
-                child: const Text('Submit Attendance'),)
+            MaterialButton(
+              onPressed: (){
+                print('Latitude: ${_userLocation?.latitude}');
+                print('Longitude: ${_userLocation?.longitude}');
+                print('Time:$_timeString');
+              },
+              child: const Text('Submit Attendance',style: TextStyle(color: Colors.white),),
+              minWidth: 150.0,
+              height: 50.0,
+              color: Colors.black,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+            ),
+            ),
 
 
-            ],
-          ),
+          ],
         ),
       ),
     );
