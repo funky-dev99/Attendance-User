4d0f95973e7a9fe9e0b8968614a3316b77060622
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-06-09 15:08:21 +0530

    Attendance Rec Page Added

---
+++ attend_user/lib/pages/attendance_rec.dart
@@ -0,0 +1,38 @@
+import 'package:attend_user/pages/profile.dart';
+import 'package:flutter/material.dart';
+
+import '../home.dart';
+
+class AttendanceRec extends StatelessWidget {
+  const AttendanceRec({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Attendance",
+          style: TextStyle(color: Colors.black, fontSize: 22),
+        ),
+        leading: IconButton(
+          icon: Image.asset("images/back.png"),
+          iconSize: 20,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) =>  HomePage()));
+          },
+        ),
+        actions: [
+          IconButton(icon: Icon(Icons.person, size: 30.0,),
+            color: Colors.black,
+            tooltip: 'View Profile',
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) =>  ProfilePage()));},)
+
+        ],
+        elevation: 0.0,
+      ),
+    );
+  }
+}
