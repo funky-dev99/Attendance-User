8971cc4f9a2b41e3308a79f368879b229df03cdf
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-14 16:08:02 +0530

    qr update

---
+++ attend_user/lib/pages/tabs/qr.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyQr extends StatelessWidget {
+  const MyQr({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('Mark In Records'),
+      ),
+    );
+  }
+}
