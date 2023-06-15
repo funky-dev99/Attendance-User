711d68d2a62ee0c11fac58fb052c9037be575e29
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-15 21:47:42 +0530

    second QR page added

--- attend_user/lib/secondUser_Pages/second_tabs/second_qr.dart
+++ attend_user/lib/secondUser_Pages/second_tabs/second_qr.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'dart:async';
+import 'dart:ui' as ui;
 
-class SecondQr extends StatelessWidget {
+import 'package:flutter/services.dart';
+
+class SecondQr extends StatefulWidget {
   const SecondQr({Key? key}) : super(key: key);
 
   @override
+  State<SecondQr> createState() => _SecondQrState();
+}
+
+class _SecondQrState extends State<SecondQr> {
+  ui.Image? image; // Declare ui.Image as a class member
+
+  @override
+  void initState() {
+    super.initState();
+    _loadImage(); // Call _loadImage in the initState method
+  }
+
+  Future<void> _loadImage() async {
+    final data = await rootBundle.load('assets/qr.png');
+    final loadedImage = await decodeImageFromList(data.buffer.asUint8List());
+    setState(() {
+      image = loadedImage;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    const message = 'My QR';
+
+    final qrCode = image != null // Use the loaded image in FutureBuilder
+        ? CustomPaint(
+      size: const Size.square(350),
+      painter: QrPainter(
+        data: message,
+        version: QrVersions.auto,
+        color: Colors.black,
+        emptyColor: Colors.white70,
+        embeddedImage: image,
+        embeddedImageStyle: QrEmbeddedImageStyle(
+          size: Size.square(50),
+        ),
+      ),
+    )
+        : Container(
+      color: Colors.teal,
+      height: 350,
+      width: 350,
+    );
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        qrCode,
+        SizedBox(height: 30.0),
+        Text(
+          message,
+          style: TextStyle(fontSize: 25.0,
+              fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+
   }
 }
+
