d4976ab6c3fe6db60e98fa31cad62ce40219d739
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-16 14:21:14 +0530

    Update payslip.dart

--- attend_user/lib/pages/payslip.dart
+++ attend_user/lib/pages/payslip.dart
@@ -1,3 +1,243 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_pdfview/flutter_pdfview.dart';
+// import 'package:url_launcher/url_launcher.dart';
+//
+// import 'home.dart';
+// import 'profile.dart';
+//
+// class PaySlipPage extends StatelessWidget {
+//   const PaySlipPage({Key? key}) : super(key: key);
+//   final double generatedSalary = 5000.0;
+//   final int takenLeave = 5;
+//   final double leaveDeduction = 100.0;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     double totalSalary = generatedSalary - (takenLeave * leaveDeduction);
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//         title: const Text(
+//           "Payment details",
+//           style: TextStyle(color: Colors.black, fontSize: 22),
+//         ),
+//         leading: IconButton(
+//           icon: Image.asset("images/back.png"),
+//           onPressed: () {
+//             Navigator.push(
+//                 context, MaterialPageRoute(builder: (context) =>  HomePage()));
+//           },
+//         ),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.person, size: 30.0,),
+//             color: Colors.black,
+//             tooltip: 'View Profile',
+//             onPressed: () {
+//               Navigator.push(context,
+//                   MaterialPageRoute(builder: (context) => const ProfilePage()));
+//             },
+//           ),
+//         ],
+//         elevation: 0.0,
+//       ),
+//       body: Column(
+//         children: [
+//           Center(
+//             child: Container(
+//               margin: const EdgeInsets.all(20.0),
+//               child: Stack(
+//                 children: [
+//                   Align(
+//                     alignment: Alignment.topLeft,
+//                     child: Container(
+//                       padding: const EdgeInsets.all(10.0),
+//                       decoration: BoxDecoration(
+//                         color: Colors.grey[350],
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.grey.withOpacity(0.5),
+//                             spreadRadius: 1,
+//                             blurRadius: 2,
+//                             offset: const Offset(0, 3),
+//                           ),
+//                         ],
+//                       ),
+//                       child: const Text(
+//                         'Month Salary',
+//                         style: TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   Positioned(
+//                     top: 0,
+//                     right: 0,
+//                     child: Container(
+//                       padding: const EdgeInsets.all(10.0),
+//                       decoration: BoxDecoration(
+//                         color: Colors.grey[350],
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.grey.withOpacity(0.5),
+//                             spreadRadius: 1,
+//                             blurRadius: 2,
+//                             offset: const Offset(0, 3),
+//                           ),
+//                         ],
+//                       ),
+//                       child: Text(
+//                         '\LKR ${generatedSalary.toStringAsFixed(2)}',
+//                         style: const TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   Align(
+//                     alignment: Alignment.centerLeft,
+//                     child: Container(
+//                       margin: const EdgeInsets.only(top: 70.0),
+//                       padding: const EdgeInsets.all(10.0),
+//                       decoration: BoxDecoration(
+//                         color: Colors.grey[350],
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.grey.withOpacity(0.5),
+//                             spreadRadius: 1,
+//                             blurRadius: 2,
+//                             offset: const Offset(0, 3),
+//                           ),
+//                         ],
+//                       ),
+//                       child: const Text(
+//                         'Taken Leave',
+//                         style: TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   Positioned(
+//                     top: 70.0,
+//                     right: 0,
+//                     child: Container(
+//                       padding: const EdgeInsets.all(10.0),
+//                       decoration: BoxDecoration(
+//                         color: Colors.grey[350],
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.grey.withOpacity(0.5),
+//                             spreadRadius: 1,
+//                             blurRadius: 2,
+//                             offset: const Offset(0, 3),
+//                           ),
+//                         ],
+//                       ),
+//                       child: Text(
+//                         takenLeave.toString(),
+//                         style: const TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   Align(
+//                     alignment: Alignment.bottomLeft,
+//                     child: Container(
+//                       margin: const EdgeInsets.only(top: 140.0),
+//                       padding: const EdgeInsets.all(10.0),
+//                       decoration: BoxDecoration(
+//                         color: Colors.grey[350],
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.grey.withOpacity(0.5),
+//                             spreadRadius: 1,
+//                             blurRadius: 2,
+//                             offset: const Offset(0, 3),
+//                           ),
+//                         ],
+//                       ),
+//                       child: const Text(
+//                         'This Month Salary',
+//                         style: TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                   Positioned(
+//                     top: 140.0,
+//                     right: 0,
+//                     child: Container(
+//                       padding: const EdgeInsets.all(10.0),
+//                       decoration: BoxDecoration(
+//                         color: Colors.grey[350],
+//                         boxShadow: [
+//                           BoxShadow(
+//                             color: Colors.grey.withOpacity(0.5),
+//                             spreadRadius: 1,
+//                             blurRadius: 2,
+//                             offset: const Offset(0, 3),
+//                           ),
+//                         ],
+//                       ),
+//                       child: Text(
+//                         '\LKR ${totalSalary.toStringAsFixed(2)}',
+//                         style: const TextStyle(
+//                           fontSize: 18.0,
+//                           fontWeight: FontWeight.w500,
+//                         ),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//       floatingActionButton: Padding(
+//         padding: const EdgeInsets.only(bottom: 60.0),
+//         child: FloatingActionButton.extended(
+//           onPressed: () {
+//             // Download logic
+//             String pdfUrl = 'https://drive.google.com/file/d/13oiEqrDQp-Wkjw7abj_8RvieqkGYKAlW/view?usp=share_link';
+//
+//             // Display PDF using flutter_pdfview package
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => PDFView(
+//                   filePath: pdfUrl,
+//                 ),
+//               ),
+//             );
+//
+//             // Alternatively, open PDF URL using url_launcher package
+//             launch(pdfUrl);
+//           },
+//           icon: const Icon(Icons.picture_as_pdf_sharp),
+//           label: const Text('Download PDF'),
+//           backgroundColor: Colors.black, // Change the background color
+//           foregroundColor: Colors.white,
+//           shape: RoundedRectangleBorder(
+//             borderRadius: BorderRadius.circular(5.0)
+//           ),
+//
+//         ),
+//       ),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -8,42 +248,64 @@ import 'profile.dart';
 class PaySlipPage extends StatelessWidget {
   const PaySlipPage({Key? key}) : super(key: key);
   final double generatedSalary = 5000.0;
-  final int takenLeave = 5;
+  final double totalReduction = 5;
   final double leaveDeduction = 100.0;
 
   @override
   Widget build(BuildContext context) {
-    double totalSalary = generatedSalary - (takenLeave * leaveDeduction);
+    double totalReductionAmount = totalReduction * leaveDeduction;
+    double totalSalary = generatedSalary - totalReductionAmount;
+
+    // Get current month and year
+    DateTime now = DateTime.now();
+    String monthYear = '${now.month}/${now.year}';
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text(
-          "Payment details",
+          "Payment Details",
           style: TextStyle(color: Colors.black, fontSize: 22),
         ),
         leading: IconButton(
           icon: Image.asset("images/back.png"),
           onPressed: () {
             Navigator.push(
-                context, MaterialPageRoute(builder: (context) =>  HomePage()));
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
           },
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.person, size: 30.0,),
+            icon: Icon(
+              Icons.person,
+              size: 30.0,
+            ),
             color: Colors.black,
             tooltip: 'View Profile',
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const ProfilePage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ProfilePage()),
+              );
             },
           ),
         ],
         elevation: 0.0,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          const SizedBox(height: 20.0),
+          Text(
+            monthYear,
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20.0),
           Center(
             child: Container(
               margin: const EdgeInsets.all(20.0),
@@ -65,7 +327,7 @@ class PaySlipPage extends StatelessWidget {
                         ],
                       ),
                       child: const Text(
-                        'Month Salary',
+                        'Total Earning',
                         style: TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.w500,
@@ -115,7 +377,7 @@ class PaySlipPage extends StatelessWidget {
                         ],
                       ),
                       child: const Text(
-                        'Taken Leave',
+                        'Total Reduction',
                         style: TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.w500,
@@ -140,7 +402,7 @@ class PaySlipPage extends StatelessWidget {
                         ],
                       ),
                       child: Text(
-                        takenLeave.toString(),
+                        '\LKR ${totalReductionAmount.toStringAsFixed(2)}',
                         style: const TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.w500,
@@ -165,7 +427,7 @@ class PaySlipPage extends StatelessWidget {
                         ],
                       ),
                       child: const Text(
-                        'This Month Salary',
+                        'Net Salary',
                         style: TextStyle(
                           fontSize: 18.0,
                           fontWeight: FontWeight.w500,
@@ -209,7 +471,8 @@ class PaySlipPage extends StatelessWidget {
         child: FloatingActionButton.extended(
           onPressed: () {
             // Download logic
-            String pdfUrl = 'https://drive.google.com/file/d/13oiEqrDQp-Wkjw7abj_8RvieqkGYKAlW/view?usp=share_link';
+            String pdfUrl =
+                'https://drive.google.com/file/d/13oiEqrDQp-Wkjw7abj_8RvieqkGYKAlW/view?usp=share_link';
 
             // Display PDF using flutter_pdfview package
             Navigator.push(
@@ -229,12 +492,12 @@ class PaySlipPage extends StatelessWidget {
           backgroundColor: Colors.black, // Change the background color
           foregroundColor: Colors.white,
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(5.0)
+            borderRadius: BorderRadius.circular(5.0),
           ),
-
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
+
