153d08eb629e24ff6d65e3f1cec709a42c33402f
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-22 16:18:36 +0530

    Update claim_req.dart

--- attend_user/lib/secondUser_Pages/second_tabs/claim_req.dart
+++ attend_user/lib/secondUser_Pages/second_tabs/claim_req.dart
@@ -241,81 +241,39 @@ class _SecClaimReqState extends State<SecClaimReq> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: SizedBox(
-        width: double.infinity,
-        child: DataTable(
-          columns: const <DataColumn>[
-            DataColumn(
-              label: Text('Claim'),
-            ),
-            DataColumn(
-              label: Text('Amount(Rs.)'),
-              numeric: true,
-            ),
-            DataColumn(
-              label: Text('Uploads'),
-            ),
-          ],
-          rows: List.generate(data.length, (index) {
-            final item = data[index];
-            return DataRow(
-              cells: [
-                DataCell(Text(item['Claim'])),
-                DataCell(Text(item['Amount'].toString())),
-                DataCell(Text(item['Uploads'])), // Fixed typo here
-              ],
-              selected: item['checked'],
-              onSelectChanged: (bool? value) {
-                setState(() {
-                  data[index]['checked'] = value!;
-                });
-                debugPrint(data.toString());
-              },
-            );
-          }),
-        ),
+    return SizedBox(
+      width: double.infinity,
+      child: DataTable(
+        columns: const <DataColumn>[
+          DataColumn(
+            label: Text('Claim'),
+          ),
+          DataColumn(
+            label: Text('Amount(Rs.)'),
+            numeric: true,
+          ),
+          DataColumn(
+            label: Text('Uploads'),
+          ),
+        ],
+        rows: List.generate(data.length, (index) {
+          final item = data[index];
+          return DataRow(
+            cells: [
+              DataCell(Text(item['Claim'])),
+              DataCell(Text(item['Amount'].toString())),
+              DataCell(Text(item['Uploads'])),
+            ],
+            selected: item['checked'],
+            onSelectChanged: (bool? value) {
+              setState(() {
+                data[index]['checked'] = value!;
+              });
+              debugPrint(data.toString());
+            },
+          );
+        }),
       ),
     );
   }
 }
-
-
-//   class SecClaimReq extends StatefulWidget {
-//   const SecClaimReq({Key? key}) : super(key: key);
-//
-//   @override
-//   State<SecClaimReq> createState() => _SecClaimReqState();
-// }
-//
-// class _SecClaimReqState extends State<SecClaimReq> {
-//   String selectedClaimType = '';
-//   final desController = TextEditingController();
-//   String uploadedFilePath = '';
-//
-//   final List<String> _claimTypes = [
-//     'Claim Type 1',
-//     'Claim Type 2',
-//     'Claim Type 3',
-//     'Claim Type 4',
-//     'Claim Type 5',
-//   ];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       color: Colors.white,
-//       padding: EdgeInsets.all(20.0),
-//       child: Table(
-//         border: TableBorder.all(color: Colors.black),
-//         children: const [
-//           TableRow(children: [
-//             Center(child: Text('Claim Type')),
-//             Center(child: Text('Amount    (Rs.)')),
-//             Center(child: Text('Uploads')),
-//           ]),
-//         ],
-//       ),
-//     );
-//   }
-// }
