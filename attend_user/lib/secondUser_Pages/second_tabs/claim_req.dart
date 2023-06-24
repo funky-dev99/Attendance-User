945c5ae687fe411a3b983ad2b8cfe9e6357f97dc
Dinethri Gunawardhana <dinethriupeksha200@gmail.com>
2023-06-24 19:18:22 +0530

    amount can change

--- attend_user/lib/secondUser_Pages/second_tabs/claim_req.dart
+++ attend_user/lib/secondUser_Pages/second_tabs/claim_req.dart
@@ -222,6 +222,7 @@
 //     );
 //   }
 // }
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 class SecClaimReq extends StatefulWidget {
@@ -232,48 +233,119 @@ class SecClaimReq extends StatefulWidget {
 }
 
 class _SecClaimReqState extends State<SecClaimReq> {
-  List<dynamic> data = [
-    {"Claim": "Travelling", "Amount": 200, "Uploads": "Upload 1", "checked": false},
-    {"Claim": "Telephone", "Amount": 200, "Uploads": "Upload 1", "checked": false},
-    {"Claim": "Claim 3", "Amount": 200, "Uploads": "Upload 1", "checked": false},
-    {"Claim": "Claim 4", "Amount": 200, "Uploads": "Upload 1", "checked": false},
+  String uploadedFilePath = '';
+  List<Map<String, dynamic>> data = [
+    {"Claim": "Travelling", "Amount": '', "Uploads": "Upload", "checked": false},
+    {"Claim": "Telephone", "Amount": '', "Uploads": "Upload", "checked": false},
+    {"Claim": "Claim 3", "Amount": '', "Uploads": "Upload", "checked": false},
+    {"Claim": "Claim 4", "Amount": '', "Uploads": "Upload", "checked": false},
   ];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: DataTable(
-        columns: const <DataColumn>[
-          DataColumn(
-            label: Text('Claim'),
-          ),
-          DataColumn(
-            label: Text('Amount(Rs.)'),
-            numeric: true,
-          ),
-          DataColumn(
-            label: Text('Uploads'),
-          ),
-        ],
-        rows: List.generate(data.length, (index) {
-          final item = data[index];
-          return DataRow(
-            cells: [
-              DataCell(Text(item['Claim'])),
-              DataCell(Text(item['Amount'].toString())),
-              DataCell(Text(item['Uploads'])),
-            ],
-            selected: item['checked'],
-            onSelectChanged: (bool? value) {
-              setState(() {
-                data[index]['checked'] = value!;
-              });
-              debugPrint(data.toString());
-            },
-          );
-        }),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Text('Claim'),
+            ),
+            DataColumn(
+              label: Text('Amount(Rs.)'),
+              numeric: true,
+            ),
+            DataColumn(
+              label: Text('Uploads'),
+            ),
+          ],
+          rows: List.generate(data.length, (index) {
+            final item = data[index];
+            return DataRow(
+              cells: [
+                DataCell(Text(item['Claim'])),
+                DataCell(
+                  TextFormField(
+                    initialValue: item['Amount'].toString(),
+                    keyboardType: TextInputType.number,
+                    onChanged: (value) {
+                      setState(() {
+                        data[index]['Amount'] = double.parse(value);
+                      });
+                    },
+                  ),
+                ),
+                DataCell(
+                  // ElevatedButton(
+                  //   onPressed: () async {
+                  //     FilePickerResult? result = await FilePicker.platform.pickFiles();
+                  //
+                  //     if (result != null) {
+                  //       PlatformFile file = result.files.first;
+                  //       setState(() {
+                  //         uploadedFilePath = file.path ?? '';
+                  //       });
+                  //
+                  //       print('File path: ${file.path}');
+                  //       print('File name: ${file.name}');
+                  //       print('File size: ${file.size}');
+                  //     } else {
+                  //       // User canceled the file picker
+                  //     }
+                  //     // print('Upload button pressed for ${item['Uploads']}');
+                  //   },
+                  //
+                  //
+                  //   child: Text(item['Uploads']),
+                  // ),
+                  ElevatedButton.icon(
+                    onPressed: () async {
+                      FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+                      if (result != null) {
+                        PlatformFile file = result.files.first;
+                        setState(() {
+                          uploadedFilePath = file.path ?? '';
+                        });
+
+                        print('File path: ${file.path}');
+                        print('File name: ${file.name}');
+                        print('File size: ${file.size}');
+                      } else {
+                        // User canceled the file picker
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.cloud_upload,
+                      color: Colors.red,
+                    ),
+                    label: const Text(
+                      'Upload',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.all(4.0), backgroundColor: Colors.grey[350],
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ), // Background color
+                    ),
+                  ),
+                ),
+              ],
+              selected: item['checked'],
+              onSelectChanged: (bool? value) {
+                setState(() {
+                  data[index]['checked'] = value!;
+                });
+                debugPrint(data.toString());
+              },
+            );
+          }),
+        ),
       ),
     );
   }
 }
+
+
